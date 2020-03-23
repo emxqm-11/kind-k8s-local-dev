ff71df83fc15a43311984acddbc458f3f2818a7a
emxqm-11 <emily.ha@contino.io>
2020-03-23 13:51:35 +1100

    delete all np task

--- testing.mk
+++ testing.mk
@@ -1,15 +1,17 @@
-nginx.deploy:
-	@kubectl run web --image=nginx --labels app=genesis --expose --port 80 -n 
 
 nginx.deploy_and_expose:
 	@kubectl apply -f ./testing/run-my-nginx.yaml
 	@kubectl expose deployment/genesis
 
+nginx.auth:
+	@kubectl apply -f ./testing/run-my-nginx-auth.yaml
+	@kubectl expose deployment/genesis -n auth
+
 busybox.tty:
 	@kubectl run busybox -it --image=busybox --restart=Never --rm
 
 busybox.label:
-	@kubectl run busybox -l app=user-api -it --image=busybox --restart=Never --rm 
+	@kubectl run busybox -l app=genesis -it --image=busybox --restart=Never --rm 
 
 busybox.coolbeans:
 	@kubectl run busybox -l app=coolbeans -it --image=busybox --restart=Never --rm 
@@ -21,8 +23,11 @@ nginx.delete:
 	@kubectl delete deployment/my-nginx
 
 hello.web:
-	@kubectl run hello-web-2 --labels app=auth \
+	@kubectl run hello-web-2 --labels app=schema-registry \
   	--image=gcr.io/google-samples/hello-app:1.0 --port 8080 --expose
 
 alpine.deploy:
 	kubectl run -l app=api-gateway --image=alpine --rm -i -t --restart=Never test-3
+
+np.delete.all:
+	k delete networkpolicy --all-namespaces --all
